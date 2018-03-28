ea3d618df2df5099698643080193dc0559aa3f96
Radhika Gupta <radhikgu@microsoft.com>
2018-03-28 10:51:04 -0700

    Switch to use the latest Ubuntu VHD by default for AzureStack.

--- pkg/acsengine/defaults.go
+++ pkg/acsengine/defaults.go
@@ -200,7 +200,7 @@ var (
 				ImageOffer:     "UbuntuServer",
 				ImageSku:       "16.04-LTS",
 				ImagePublisher: "Canonical",
-				ImageVersion:   "16.04.201802220",
+				ImageVersion:   "latest",
 			},
 			api.RHEL: DefaultRHELOSImageConfig,
 		},
